d0902598409cba601d1c9f45bdc63a1e5e5ab3d5
SanketDhabarde <sanketdhabarde13@gmail.com>
2022-09-22 17:03:20 +0530

    [Feat]: Move the lift doors

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,12 @@ let floorLiftControllers = null;
 let allLifts = [];
 const allLiftState = [];
 
+const DOOR_STATE = {
+  closing: -1,
+  none: 0,
+  opening: 1,
+};
+
 let noOfFloors = 0;
 let noOfLifts = 0;
 
@@ -38,11 +44,16 @@ const generateLifts = (noOfLifts) => {
     lift.classList.add("lift");
     lift.setAttribute("data-currentFloor", 1);
     lift.setAttribute("data-liftNo", i + 1);
+    lift.innerHTML = `
+    <div class="left-door"></div>
+    <div class="right-door"></div>
+    `;
     lift.style.left = 15 * (i + 1) + `%`;
     liftEles.appendChild(lift);
     const liftState = {
       LiftNo: i + 1,
       isMoving: false,
+      doorState: DOOR_STATE.none,
     };
     allLiftState.push(liftState);
   }
@@ -96,18 +107,68 @@ const moveLiftHandler = (e) => {
   const down = e.target.dataset.down;
   const liftIndex = getFreeLiftIndex();
   const currentLift = allLifts[liftIndex];
+  const liftDurationPerFloor =
+    Math.abs(currentLift.dataset.currentfloor - (up ?? down)) * 2; // 2 sec per floor
 
   if (currentLift) {
     allLiftState[liftIndex].isMoving = true;
-    currentLift.style.bottom = ((up ?? down) - 1) * 96 + "px";
+    currentLift.style.transitionDuration = liftDurationPerFloor + "s";
+    currentLift.style.transitionTimingFunction = "linear";
+    currentLift.style.bottom = ((up ?? down) - 1) * 97 + "px";
+
     setTimeout(() => {
       allLiftState[liftIndex].isMoving = false;
+      doorOpenHandler(currentLift, liftIndex).then(({ lift, liftIndex }) =>
+        doorCloseHandler(lift, liftIndex)
+      );
       currentLift.dataset.currentfloor = up ?? down;
-    }, 5000);
+    }, liftDurationPerFloor * 1000);
   }
 };
 
-const getFreeLiftIndex = () => allLiftState.findIndex((lift) => !lift.isMoving);
+const doorCloseHandler = (lift, liftIndex) => {
+  allLiftState[liftIndex].doorState = DOOR_STATE.closing;
+  doorSimulation(lift.children, DOOR_STATE.closing);
+  
+  setTimeout(() => {
+    allLiftState[liftIndex].doorState = DOOR_STATE.none;
+  }, 2500);
+};
+
+const doorOpenHandler = (lift, liftIndex) => {
+  return new Promise((resolve, reject) => {
+    allLiftState[liftIndex].doorState = DOOR_STATE.opening;
+    doorSimulation(lift.children, DOOR_STATE.opening);
+    
+    setTimeout(() => {
+      resolve({ lift, liftIndex });
+    }, 2500);
+  });
+};
+
+const doorSimulation = (doors, state) => {
+  const [leftDoor, rightDoor] = doors;
+  switch (state) {
+    case DOOR_STATE.opening:
+      {
+        leftDoor.style.transform = `translateX(-100%)`;
+        rightDoor.style.transform = `translateX(100%)`;
+      }
+      break;
+    case DOOR_STATE.closing:
+      {
+        leftDoor.style.transform = `translateX(0%)`;
+        rightDoor.style.transform = `translateX(0%)`;
+      }
+      break;
+    default:
+  }
+};
+
+const getFreeLiftIndex = () =>
+  allLiftState.findIndex(
+    (lift) => !lift.isMoving && lift.doorState === DOOR_STATE.none
+  );
 
 noOfFloorsInput.addEventListener("input", (event) =>
   inputChangeHandler(event, "floorChange")
